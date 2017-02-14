4d51acce7814ec1a02887ad038c8fa5f6d801bd7
Michael Shepherd <michael123gs@aol.com>
2017-02-14 17:48:00 +0000

    Added Tasks

---
+++ DoIt/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Michael Shepherd on 14/02/2017.
+//  Copyright © 2017 Zappy Code. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    
+    var name = ""
+    var important = false
+    
+}
